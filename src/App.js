3dd63b5a7c7cdc4567da36ccbc102b23b485d545
muz110 <muzafardba@gmail.com>
2020-12-28 11:05:04 +0400

    initial project commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p> An amazing startup idea Muntazir Jatoi's is Amazing Idea of life </p>
+        <p> Production Server up and Running Wooo </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
